645b6671a88ae777516ed12260cf1e1317cdb139
superryanguo <superryanguo@gmail.com>
2017-10-08 18:01:32 +0800

    No database, no app

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # homePostBoard
 A web app to help you to build your post board for your family member@home, share info, add e-tips and record something.
+## Introduction
+### DB dependency
+on Ubuntu:
+```
+sudo apt-get install sqlite3
+```
+on Mac:
+```
+brew install sqlite3
+```
+## License
+GNU Lesser General Public License v3.0
--- homePostBoard.go
+++ homePostBoard.go
@@ -1,20 +1,52 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+//TODO:UID is for IP and Name is for requestBody
 type PostData struct {
-	userId string
-	post   string
+	UId      int
+	UserName string
+	Content  string
+	Created  string
 }
 
+var database *sql.DB
+var err error
+
+func init() {
+	database, err =
+		sql.Open("sqlite3", "./postData.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sql_table := `
+    CREATE TABLE IF NOT EXISTS postdata(
+        uid INTEGER PRIMARY KEY AUTOINCREMENT,
+        username VARCHAR(64) NULL,
+        content VARCHAR(3000) NULL,
+        created DATE NULL
+    );
+    `
+	_, err := database.Exec(sql_table)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "RequestURI: %s\n", r.RequestURI)
+	fmt.Fprintf(w, "RequestBody: %s\n", r.Body)
 }
 func main() {
+	http.Handle("/static/", http.FileServer(http.Dir("public")))
 	http.HandleFunc("/", rootHandler)
+	log.Print("Running the server on port 8091.")
 	log.Fatal(http.ListenAndServe(":8091", nil))
 }
