f42eda4505e63616fdcf834ae9a170663237e851
superryanguo <superryanguo@gmail.com>
2017-10-13 22:03:06 +0800

    add and show the post complete

--- homePostBoard.go
+++ homePostBoard.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -17,10 +17,32 @@ type PostData struct {
 	Content  string
 	Created  string
 }
+type PostContext struct {
+	Context []PostData
+}
 
 var database *sql.DB
 var err error
 
+func (p *PostData) WriteDb() {
+	stmt, err := database.Prepare("INSERT INTO postdata(username, content, created) VALUES(?,?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := stmt.Exec(p.UserName, p.Content, p.Created)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lastId, err := res.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+	rowCnt, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
+}
 func init() {
 	database, err =
 		sql.Open("sqlite3", "./postData.db")
@@ -41,21 +63,49 @@ func init() {
 		log.Fatal(err)
 	}
 }
+func showPostBoard(pattern string, w http.ResponseWriter) (err error) {
+	sqlStr := "SELECT " + pattern + " FROM postdata"
+	rows, err := database.Query(sqlStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var c PostContext
+	var uid int
+	var username string
+	var content string
+	var created string
+
+	for rows.Next() {
+		err = rows.Scan(&uid, &username, &content, &created)
+		if err != nil {
+			log.Fatal(err)
+		}
+		p := PostData{UId: uid, UserName: username, Content: content, Created: created}
+		c.Context = append(c.Context, p)
+	}
+
+	rows.Close()
+
+	t, err := template.ParseFiles("./templates/board.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, c)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	return
+}
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "RequestURI: %s\n", r.RequestURI)
 	// fmt.Fprintf(w, "RequestRemoteAddr: %s\n", r.RemoteAddr)
 	// fmt.Fprintf(w, "RequestHeader: %s\n", r.Header)
 	if r.Method == "GET" {
-		p := PostData{UId: 1, UserName: "Ryan", Content: "The First post from Ryan", Created: "20171010"}
-		t, err := template.ParseFiles("./templates/board.html")
+		err = showPostBoard("*", w)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		err = t.Execute(w, p)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+			log.Fatal(err)
 		}
 	} else if r.Method == "POST" {
 		err = r.ParseForm()
@@ -63,8 +113,14 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		fmt.Fprintf(w, "RequestBody: %s\n", r.Form["body"])
+		t := time.Now().Format("2006-01-02 15:04:05")
+		p := PostData{UserName: "guo", Content: r.Form["body"][0], Created: t}
+		p.WriteDb()
 
+		err = showPostBoard("*", w)
+		if err != nil {
+			log.Fatal(err)
+		}
 	} else {
 		http.Error(w, "Unknown HTTP Action", http.StatusInternalServerError)
 		return
